3e293f1dc29fb677616d184741771fe40688f64f
jiangshang <jiangshangvip@yeah.net>
2018-05-14 21:45:38 +0800

    旋转

--- Assets/Script/Controll/ImageViewer.cs
+++ Assets/Script/Controll/ImageViewer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -11,7 +12,7 @@ namespace Assets.Controll
     /// <summary>
     /// 使用image展示3d对象。原对象不受影响
     /// </summary>
-    public class ImageViewer:MonoBehaviour
+    public class ImageViewer:EventTrigger
     {
         public int renderWidth = 256;
         public int renderHeight = 256;
@@ -23,12 +24,13 @@ namespace Assets.Controll
         private GameObject cameraObject;
         private new Camera camera;
         public LayerMask layerMask;
+        public GameObject target;
 
         public Camera Camera
         {
             get
             {
-                return camera.GetComponent<Camera>();
+                return cameraObject.GetComponent<Camera>();
             }
         }
 
@@ -58,6 +60,38 @@ namespace Assets.Controll
             image = GetComponent<Image>();
         }
 
+        private Vector3 startPos;
+        private Vector3 offset = new Vector3();
+        private Vector3 axis;
+        private bool isDraging;
+        private float speedRotate = -Mathf.PI;
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+            startPos = Input.mousePosition;
+            isDraging = true;
+
+        }
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            base.OnPointerUp(eventData);
+            isDraging = false;
+        }
+
+        private void Update()
+        {
+            if (isDraging)
+            {
+                offset = Input.mousePosition - startPos;
+                startPos = Input.mousePosition;
+                offset.z = 0;
+                axis = Vector3.Cross(offset, Camera.transform.position);
+                target.transform.Rotate(axis,speedRotate,Space.World);
+                Debug.LogWarning("i am droging");
+            }
+        }
+
         private void OnDestroy()
         {
             if (scene.IsValid())
@@ -69,14 +103,16 @@ namespace Assets.Controll
 
         public void AddTarget(Transform trans)
         {
-            
-            GameObject gameObject = GameObject.Instantiate(trans.gameObject);
-            gameObject.layer = SortingLayer.GetLayerValueFromName("ImageViewer");
+
+            target = GameObject.Instantiate(trans.gameObject);
+            target.layer = SortingLayer.GetLayerValueFromName("ImageViewer");
             if (scene.IsValid())
             {
-                SceneManager.MoveGameObjectToScene(gameObject, scene);
+                SceneManager.MoveGameObjectToScene(target, scene);
             }
         }
+
+
 
         public void RemoveTarget(Transform trans)
         {
